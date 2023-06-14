38be8686c6bc6b53cea6c226997e57ab3d07de84
tbyronnc <terry.byron@gmail.com>
2023-06-14 08:28:51 -0400

    updated

--- bin/cos-info.cmd
+++ bin/cos-info.cmd
@@ -12,3 +12,5 @@ set /p IP=<%CONFDIR%\IP.txt
 echo HostName:     %COMPUTERNAME% > %USERPROFILE%\Desktop\COS-INFO.txt
 echo Tag:          %TAG%        >> %USERPROFILE%\Desktop\COS-INFO.txt
 echo IP:           %IP%         >> %USERPROFILE%\Desktop\COS-INFO.txt
+type $USERPROFILE%\Desktop\COS-INFO.txt
+timeout /t 20
