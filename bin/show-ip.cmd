be46b6d7b411d84ea5f22f22f7e1f583cbf254ba
tbyronnc <terry.byron@gmail.com>
2023-06-16 11:03:06 -0400

    Updated

--- bin/show-ip.cmd
+++ bin/show-ip.cmd
@@ -1,4 +1,4 @@
 @echo off
-# netsh interface ip show address "Ethernet" | findstr "IP Address"
+REM netsh interface ip show address "Ethernet" | findstr "IP Address"
 ipconfig | findstr "IPv4"
 timeout /t 20
