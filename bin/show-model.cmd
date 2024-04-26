45efef4361271519f6a445bd922b654846be1ab2
tbyronnc <terry.byron@gmail.com>
2024-04-26 16:10:27 -0400

    Updated

--- bin/show-model.cmd
+++ bin/show-model.cmd
@@ -2,5 +2,6 @@
 FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
 REM echo %model%
 set model=%model: =_%
+echo %model% >> c:\opt\cos\etc\MODEL.txt
 echo %model%
 timeout /t 20
