49e1a00aeca6dd5a88c87212d63a65652ae3a008
tbyronnc <terry.byron@gmail.com>
2024-04-26 16:09:19 -0400

    Updated

--- bin/show-model.cmd
+++ bin/show-model.cmd
@@ -1,3 +1,6 @@
 @echo off
-wmic computersystem get model | findstr /v "Model"
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
+REM echo %model%
+set model=%model: =_%
+echo %model%
 timeout /t 20
