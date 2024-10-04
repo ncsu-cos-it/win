0b54a702bff25712086d672fdab3f73ceb6c8162
tbyronnc <terry.byron@gmail.com>
2024-10-04 14:57:03 -0400

    Updated

--- bin/show-cores.cmd
+++ bin/show-cores.cmd
@@ -1,5 +1,5 @@
 @echo off
 set BINDIR="C:\opt\cos\bin"
 set CONFDIR="C:\opt\cos\etc"
-powershell -command %BINDIR%\core-set.ps1 
-timeout /t /20
+powershell -command %BINDIR%\cores-set.ps1 
+timeout /t 20
