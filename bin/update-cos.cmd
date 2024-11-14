1d5eb52d4eb2a04b1bf8d7f8b0fb3b5bf1829e37
tbyronnc <terry.byron@gmail.com>
2024-11-14 16:12:49 -0500

    Updated'

--- bin/update-cos.cmd
+++ bin/update-cos.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -noprofile -executionpolicy bypass -command C:\opt\cos\bin\update-cos.ps1
+powershell -noprofile -executionpolicy bypass -command \\wolftech.ad.ncsu.edu\cos\stat\files\it_pub\update-cos.ps1
