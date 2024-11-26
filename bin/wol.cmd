7acc9e412ef83e710eebec25880be312e356612e
tbyronnc <terry.byron@gmail.com>
2024-11-26 13:02:01 -0500

    Updated

--- bin/wol.cmd
+++ bin/wol.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -noprofile -executionpolicy bypass -command c:\opt\cos\bin\wol.ps1
+powershell -noprofile -executionpolicy bypass -command c:\opt\cos\bin\wol.ps1 %~1 %~2 %~3 %~4
