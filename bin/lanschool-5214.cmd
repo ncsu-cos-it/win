33e76f52a77fe255b822c0d85f177fa4f3242579
tbyronnc <terry.byron@gmail.com>
2025-05-01 14:35:05 -0400

    Updated

---
+++ bin/lanschool-5214.cmd
@@ -0,0 +1,3 @@
+@echo off
+msiexec /i \\wolftech.ad.ncsu.edu\cos\software\lanschool\v9.3.056\Windows\Student.msi /qn CHANNEL=5214 ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1 INSTALL_CHROME_EXTENSION=0 INSTALL_EDGE_EXTENSION=0
+
