9653ccc4565fba4dc447779e3538b98140f7246d
tbyronnc <terry.byron@gmail.com>
2025-05-12 12:48:50 -0400

    Updated

---
+++ bin/chemdraw.cmd
@@ -0,0 +1,6 @@
+@echo off
+msiexec /i \\wolftech.ad.ncsu.edu\cos\software\Revvity-ChemDraw23\Revvity_ChemDraw_Suite_23.1.2_x64.msi /q 
+REM copy \\wolftech.ad.ncsu.edu\cos\software\Revvity-ChemDraw23\Revvity\Activation\Activate.ini c:\ProgramData\RevvitySignalsSoftware\FNE
+\\wolftech.ad.ncsu.edu\cos\software\Revvity-ChemDraw23\Revvity\Activation\Activate.exe 22.0 IsInstaller
+
+
