1a482e5175b6e54f3bcb1e36bfcf8ac09147c084
tbyronnc <terry.byron@gmail.com>
2025-05-30 08:47:25 -0400

    set-tag.cmd

--- bin/set-tag.cmd
+++ bin/set-tag.cmd
@@ -1,15 +1,15 @@
 @echo off
 set BINDIR="C:\opt\cos\bin"
 set CONFDIR="C:\opt\cos\etc"
-if EXIST "%CONFDIR%\Tag.txt" GOTO :END
-powershell -noprofile -executionpolicy bypass -command %BINDIR%\sn-set.ps1 > %CONFDIR%\SN.txt
-:SNFOUND
 set /p SN=<%CONFDIR%\SN.txt
+if EXIST "%CONFDIR%\Tag.txt" GOTO :END
+if EXIST "%CONFDIR%\SN.txt" GOTO :SNFOUND
+powershell -noprofile -executionpolicy bypass -command %BINDIR%\sn-set.ps1 > %CONFDIR%\SN.txt
 set SNIPEINFO=%TEMP%\%SN%.txt"
 powershell -noprofile -executionpolicy bypass -command %BINDIR%\sn-query.ps1 %SN% > %SNIPEINFO%
 
 REM If %SNIPEINFO%  Size = 0 GOTO :VPNERROR
-
+:SNFOUND
 powershell -noprofile -executionpolicy bypass -command %BINDIR%\sn-asset.ps1 %SN% 
 del /q %SNIPEINFO%
   
