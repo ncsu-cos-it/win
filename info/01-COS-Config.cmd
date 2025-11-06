22d8e250b1b521f1040e7fea248a12d870b00e38
tbyronnc <terry.byron@gmail.com>
2025-11-06 08:57:20 -0500

    updated

--- info/01-COS-Config.cmd
+++ info/01-COS-Config.cmd
@@ -8,4 +8,3 @@ call show-model
 call show-tag
 cd \opt\info
 echo Move shortcuts in c:\opt\cos\info to C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
-echo Double click c:\Opt\Scrnssavr\wscreen-3840x2160.reg
