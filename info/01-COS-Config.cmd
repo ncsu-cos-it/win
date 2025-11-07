581f199070d42cdf3d17c1a10d764d7caee415b7
tbyronnc <terry.byron@gmail.com>
2025-11-07 09:32:17 -0500

    Updated

--- info/01-COS-Config.cmd
+++ info/01-COS-Config.cmd
@@ -6,5 +6,6 @@ call make-temp
 call show-sn
 call show-model
 call show-tag
+setx PATH %PATH%;c:\opt\cos\bin /M
 cd \opt\info
 echo Move shortcuts in c:\opt\cos\info to C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
