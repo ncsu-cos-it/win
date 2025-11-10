fb3c83a58034e3e4426c28fe6336d634a466ddc3
tbyronnc <terry.byron@gmail.com>
2025-11-10 08:51:36 -0500

    Updated

--- bin/show-mac.cmd
+++ bin/show-mac.cmd
@@ -1,5 +1,5 @@
 @echo off
-for /f "tokens=*" %%g IN (getmac | findstr "\-" | findstr /r "\Device" ) do (SET MACADDR=%%g)
+for /f "tokens=*" %%g IN ( getmac | findstr "\-" | findstr /r "\Device" ) do (SET MACADDR=%%g)
 #for /f %%G in ("%MACADDR") do (
 #	set "MAC=%%G"
 #)
