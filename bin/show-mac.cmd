0364ebec0833b274a63d6f89db01b450bf10ba51
tbyronnc <terry.byron@gmail.com>
2025-11-10 09:13:58 -0500

    updated

--- bin/show-mac.cmd
+++ bin/show-mac.cmd
@@ -4,7 +4,7 @@ getmac | findstr "\-" | findstr /r "\Device" > %TEMPFILE%
 set /p MACADDR=<%TEMPFILE%
 REM del %TEMPFILE%
 REM set TEMPFILE
-for /f %%G in ("%MACADDR") do (
+for /f %%G in ("%MACADDR%") do (
 set "MAC=%%G"
 )
 echo %MACADDR%
