d40698857930e60a825be11e64ab2afaa9f2eb11
tbyronnc <terry.byron@gmail.com>
2025-11-13 15:29:49 -0500

    udpated

--- bin/show-dell-warranty.cmd
+++ bin/show-dell-warranty.cmd
@@ -2,10 +2,11 @@
 
 set BINDIR="C:\opt\cos\bin"
 set CONFDIR="C:\opt\cos\etc"
+echo Variable=%1
 if "%1" == "" (
    set /p SN=<%CONFDIR%\SN.txt
 ) ELSE (
-   set /p SN=%1
+   set /p SN=<%1
 )
 powershell -noprofile -executionpolicy bypass -command %BINDIR%\dell-warranty-info.ps1 -ServiceTag %SN%
 
