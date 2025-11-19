56fd772d9a5414108335c35e8ce926e8311491e2
tbyronnc <terry.byron@gmail.com>
2025-11-19 07:09:56 -0500

    Updated

--- sbin/user-add-remotedesktop.ps1
+++ sbin/user-add-remotedesktop.ps1
@@ -1,7 +1,7 @@
 ﻿$UnityID = Read-Host "Enter Unity Id of user to be allowed RemoteDeesktop access" | Write-Output
 
 $ADDomain = $env:USERDOMAIN
-
-Add-LocalGroupMember -Group "Users" -Member $ADDomain\$UnityID           
+write-host "$ADDOMAIN\$UnityID"
+# Add-LocalGroupMember -Group "Users" -Member $ADDomain\$UnityID           
 #Add-LocalGroupMember -Group "Remote Desktop Users" -Member $ADDomain\$UnityID           
 write-host "$UnityID added to Users and Remote Desktop Users."
